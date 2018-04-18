45faec366866e5db89db5d6206a8efacf953ac01
jiang7552261@163.com <jiang7552261@163.com>
2018-04-18 09:56:48 +0800

    调整利率

--- src/Model/BusinessSet.php
+++ src/Model/BusinessSet.php
@@ -34,13 +34,13 @@ class BusinessSet extends Model
     public function Seeder(){
         DB::statement('TRUNCATE `'.config('database.connections.mysql.prefix').'lbb_business_set`');
         $business = [
-            //定存宝利息
+            //定存宝利息 单位是%
             'financial_limit' => [
-                '1' => ['index' => 1, 'date' => 30, 'rate' => 0.004],
-                '2' => ['index' => 2, 'date' => 60, 'rate' => 0.006],
-                '3' => [ 'index' => 3, 'date' => 90, 'rate' => 0.008],
+                '1' => ['index' => 1, 'date' => 30, 'rate' => 0.4],
+                '2' => ['index' => 2, 'date' => 60, 'rate' => 0.6],
+                '3' => [ 'index' => 3, 'date' => 90, 'rate' => 0.8],
             ],
-            'balance_rate' => 0.002, //余额利息
+            'balance_rate' => 0.2, //余额利息
             /*推广收益*/
             'promote' => [
                 //是否开启
